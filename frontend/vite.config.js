ed319a34bb8d35242f4f59f248b1bc6186503a37
stevekahanya <wakahusteve@gmail.com>
2025-11-27 16:44:23 +0300

    fix dom erro22rfgf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'es2020' // Fixes "import.meta is not available" warning
+    outDir: 'dist',
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: 'es2020'
-    }
-  }
-})
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
